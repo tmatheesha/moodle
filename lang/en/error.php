332c8e69967ebe9e3325d0096e935d7cac951c99
Adrian Greeve <adrian@moodle.com>
2012-11-13 09:42:45 +0800

    MDL-31750 - course: Creating a consistent api for checking permissions for moving courses to different categories.

--- lang/en/error.php
+++ lang/en/error.php
@@ -103,6 +103,7 @@ $string['cannotmarktopic'] = 'Could not mark that topic for this course';
 $string['cannotmigratedatacomments'] = 'Cannot migrate data module comments';
 $string['cannotmodulename'] = 'Cannot get the module name in build navigation';
 $string['cannotmoduletype'] = 'Cannot get the module type in build navigation';
+$string['cannotmovecoursetocategory'] = 'You can not move this course to the category specified';
 $string['cannotmoverolewithid'] = 'Cannot move role with ID {$a}';
 $string['cannotnetgeo'] = 'Cannot connect to NetGeo server at http://netgeo.caida.org, please check proxy settings or better install MaxMind GeoLite City data file';
 $string['cannotopencsv'] = 'Cannot open CSV file';
