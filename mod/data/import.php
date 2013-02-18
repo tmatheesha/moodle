7de5cd0add0828f79b453c0138a0f573d3de23b9
Adrian Greeve <adrian@moodle.com>
2013-02-18 13:12:17 +0800

    MDL-37503 - mod_data: Adding a preview page for the database module.

--- mod/data/import.php
+++ mod/data/import.php
@@ -32,7 +32,8 @@ $id              = optional_param('id', 0, PARAM_INT);  // course module id
 $d               = optional_param('d', 0, PARAM_INT);   // database id
 $rid             = optional_param('rid', 0, PARAM_INT); // record id
 $fielddelimiter  = optional_param('fielddelimiter', ',', PARAM_CLEANHTML); // characters used as field delimiters for csv file import
-$fieldenclosure = optional_param('fieldenclosure', '', PARAM_CLEANHTML);   // characters used as record delimiters for csv file import
+$fieldenclosure  = optional_param('fieldenclosure', '', PARAM_CLEANHTML);   // characters used as record delimiters for csv file import
+$iid             = optional_param('iid', null, PARAM_INT); // Import ID.
 
 $url = new moodle_url('/mod/data/import.php');
 if ($rid !== 0) {
@@ -64,133 +65,194 @@ require_login($course, false, $cm);
 
 $context = context_module::instance($cm->id);
 require_capability('mod/data:manageentries', $context);
-$form = new mod_data_import_form(new moodle_url('/mod/data/import.php'));
 
-/// Print the page header
-$PAGE->navbar->add(get_string('add', 'data'));
-$PAGE->set_title($data->name);
-$PAGE->set_heading($course->fullname);
-navigation_node::override_active_url(new moodle_url('/mod/data/import.php', array('d' => $data->id)));
-echo $OUTPUT->header();
-echo $OUTPUT->heading_with_help(get_string('uploadrecords', 'mod_data'), 'uploadrecords', 'mod_data');
+$returnurl = new moodle_url('/mod/data/view.php', (array('id' => $cm->id)));
 
-/// Groups needed for Add entry tab
-$currentgroup = groups_get_activity_group($cm);
-$groupmode = groups_get_activity_groupmode($cm);
+$forminfo    = new stdClass();
+$forminfo->d = $data->id;
 
-if (!$formdata = $form->get_data()) {
-    /// Upload records section. Only for teachers and the admin.
-    echo $OUTPUT->box_start('generalbox boxaligncenter boxwidthwide');
-    require_once('import_form.php');
+if (empty($iid)) {
     $form = new mod_data_import_form(new moodle_url('/mod/data/import.php'));
-    $formdata = new stdClass();
-    $formdata->d = $data->id;
-    $form->set_data($formdata);
-    $form->display();
-    echo $OUTPUT->box_end();
-    echo $OUTPUT->footer();
-    die;
-} else {
-    // Large files are likely to take their time and memory. Let PHP know
-    // that we'll take longer, and that the process should be recycled soon
-    // to free up memory.
-    @set_time_limit(0);
-    raise_memory_limit(MEMORY_EXTRA);
 
-    $iid = csv_import_reader::get_new_iid('moddata');
-    $cir = new csv_import_reader($iid, 'moddata');
+    // Print the page header.
+    $PAGE->navbar->add(get_string('add', 'data'));
+    $PAGE->set_title($data->name);
+    $PAGE->set_heading($course->fullname);
+    navigation_node::override_active_url(new moodle_url('/mod/data/import.php', array('d' => $data->id)));
+    echo $OUTPUT->header();
+    echo $OUTPUT->heading_with_help(get_string('uploadrecords', 'mod_data'), 'uploadrecords', 'mod_data');
 
-    $filecontent = $form->get_file_content('recordsfile');
-    $readcount = $cir->load_csv_content($filecontent, $formdata->encoding, $formdata->fielddelimiter);
-    unset($filecontent);
-    if (empty($readcount)) {
-        print_error('csvfailed','data',"{$CFG->wwwroot}/mod/data/edit.php?d={$data->id}");
+    // Groups needed for Add entry tab.
+    $currentgroup = groups_get_activity_group($cm);
+    $groupmode = groups_get_activity_groupmode($cm);
+
+    if ($formdata = $form->get_data()) {
+
+        // Large files are likely to take their time and memory. Let PHP know
+        // that we'll take longer, and that the process should be recycled soon
+        // to free up memory.
+        @set_time_limit(0);
+        raise_memory_limit(MEMORY_EXTRA);
+
+        $iid = csv_import_reader::get_new_iid('moddata');
+        $csvimporter = new csv_import_reader($iid, 'moddata');
+        $filecontent = $form->get_file_content('recordsfile');
+        $readcount = $csvimporter->load_csv_content($filecontent, $formdata->encoding, $formdata->fielddelimiter);
+        $errormessage = $csvimporter->get_error();
+        print_object($errormessage);
+        unset($filecontent);
+
+        $headers = $csvimporter->get_columns();
+        $header = array();
+        foreach ($headers as $key => $headerdata) {
+            $headerdata = trim($headerdata); // Remove whitespace.
+            $headerdata = clean_param($headerdata, PARAM_RAW); // Clean the header.
+            $header[$key] = $headerdata;
+        }
+
+        $table = new html_table();
+        $table->id = "udbmpreview";
+        $table->attributes['class'] = 'generaltable';
+        $table->tablealign = 'center';
+        $table->head = $header;
+        $csvimporter->init();
+        $previewdata = array();
+        // Print a preview of the data.
+        $numlines = 0; // 0 lines previewed so far.
+        while ($numlines <= $formdata->previewrows) {
+            $lines = $csvimporter->next();
+            if ($lines) {
+                $previewdata[] = $lines;
+            }
+            $numlines ++;
+        }
+        $table->data = $previewdata;
+
+        echo html_writer::tag('div', html_writer::table($table), array('class'=>'flexible-wrap'));
+
+        $forminfo->iid = $iid;
+
     } else {
-        if (!$fieldnames = $cir->get_columns()) {
-            print_error('cannotreadtmpfile', 'error');
-        }
-        // check the fieldnames are valid
-        $fields = $DB->get_records('data_fields', array('dataid'=>$data->id), '', 'name, id, type');
-        $errorfield = '';
-        foreach ($fieldnames as $name) {
-            if (!isset($fields[$name])) {
-                $errorfield .= "'$name' ";
-            }
-        }
-
-        if (!empty($errorfield)) {
-            print_error('fieldnotmatched','data',"{$CFG->wwwroot}/mod/data/edit.php?d={$data->id}",$errorfield);
-        }
-
-        $cir->init();
-        $recordsadded = 0;
-        while ($record = $cir->next()) {
-            if ($recordid = data_add_record($data, 0)) {  // add instance to data_record
-                $fields = $DB->get_records('data_fields', array('dataid'=>$data->id), '', 'name, id, type');
-
-                // Insert new data_content fields with NULL contents:
-                foreach ($fields as $field) {
-                    $content = new stdClass();
-                    $content->recordid = $recordid;
-                    $content->fieldid = $field->id;
-                    $DB->insert_record('data_content', $content);
-                }
-                // Fill data_content with the values imported from the CSV file:
-                foreach ($record as $key => $value) {
-                    $name = $fieldnames[$key];
-                    $field = $fields[$name];
-                    $content = new stdClass();
-                    $content->fieldid = $field->id;
-                    $content->recordid = $recordid;
-                    if ($field->type == 'textarea') {
-                        // the only field type where HTML is possible
-                        $value = clean_param($value, PARAM_CLEANHTML);
-                    } else {
-                        // remove potential HTML:
-                        $patterns[] = '/</';
-                        $replacements[] = '&lt;';
-                        $patterns[] = '/>/';
-                        $replacements[] = '&gt;';
-                        $value = preg_replace($patterns, $replacements, $value);
-                    }
-                    // for now, only for "latlong" and "url" fields, but that should better be looked up from
-                    // $CFG->dirroot . '/mod/data/field/' . $field->type . '/field.class.php'
-                    // once there is stored how many contents the field can have.
-                    if ($field->type == 'latlong') {
-                        $values = explode(" ", $value, 2);
-                        // The lat, long values might be in a different float format.
-                        $content->content  = unformat_float($values[0]);
-                        $content->content1 = unformat_float($values[1]);
-                    } else if ($field->type == 'url') {
-                        $values = explode(" ", $value, 2);
-                        $content->content  = $values[0];
-                        // The url field doesn't always have two values (unforced autolinking).
-                        if (count($values) > 1) {
-                            $content->content1 = $values[1];
-                        }
-                    } else {
-                        $content->content = $value;
-                    }
-                    $oldcontent = $DB->get_record('data_content', array('fieldid'=>$field->id, 'recordid'=>$recordid));
-                    $content->id = $oldcontent->id;
-                    $DB->update_record('data_content', $content);
-                }
-                $recordsadded++;
-                print get_string('added', 'moodle', $recordsadded) . ". " . get_string('entry', 'data') . " (ID $recordid)<br />\n";
-            }
-        }
-        $cir->close();
-        $cir->cleanup(true);
+        echo $OUTPUT->box_start('generalbox boxaligncenter boxwidthwide');
+        $form->set_data($forminfo);
+        $form->display();
+        echo $OUTPUT->box_end();
+        echo $OUTPUT->footer();
+        die();
     }
 }
 
-if ($recordsadded > 0) {
-    echo $OUTPUT->notification($recordsadded. ' '. get_string('recordssaved', 'data'), '');
-} else {
-    echo $OUTPUT->notification(get_string('recordsnotsaved', 'data'), 'notifysuccess');
+// Large files are likely to take their time and memory. Let PHP know
+// that we'll take longer, and that the process should be recycled soon
+// to free up memory.
+@set_time_limit(0);
+raise_memory_limit(MEMORY_EXTRA);
+
+$cir = new csv_import_reader($iid, 'moddata');
+if (!$header = $cir->get_columns()) {
+        print_error('cannotreadtmpfile', 'error');
 }
 
-echo $OUTPUT->continue_button('import.php?d='.$data->id);
+$fields = $DB->get_records('data_fields', array('dataid'=>$data->id), '', 'name, id, type');
+$form2 = new mod_data_import_form2(null, array('columns' => $header, 'fields' => $fields, 'formdata' => $forminfo));
 
-/// Finish the page
+if ($formdata = $form2->is_cancelled()) {
+    $cir->cleanup(true);
+    redirect($returnurl);
+}
+
+if ($formdata = $form2->get_data()) {
+
+    $PAGE->navbar->add(get_string('add', 'data'));
+    $PAGE->set_title($data->name);
+    $PAGE->set_heading($course->fullname);
+    navigation_node::override_active_url(new moodle_url('/mod/data/import.php', array('d' => $data->id)));
+    echo $OUTPUT->header();
+    echo $OUTPUT->heading_with_help(get_string('uploadrecords', 'mod_data'), 'uploadrecords', 'mod_data');
+
+    // Groups needed for Add entry tab.
+    $currentgroup = groups_get_activity_group($cm);
+    $groupmode = groups_get_activity_groupmode($cm);
+
+    $fields = $DB->get_records('data_fields', array('dataid'=>$data->id), '', 'name, id, type');
+
+    $pattern = '/fieldmap_/';
+    $fieldinfo = array();
+    foreach ($formdata as $key => $value) {
+        if (preg_match($pattern, $key)) {
+            $fieldname = explode('_', $key);
+            if ($value != 'none') {
+                $fieldinfo[$value] = $fieldname[1];
+            }
+        }
+    }
+
+    $cir->init();
+    $recordsadded = 0;
+    while ($record = $cir->next()) {
+        if ($recordid = data_add_record($data, 0)) {  // add instance to data_record.
+
+            // Insert new data_content fields with NULL contents:
+            foreach ($fields as $field) {
+                $content = new stdClass();
+                $content->recordid = $recordid;
+                $content->fieldid = $field->id;
+                $DB->insert_record('data_content', $content);
+            }
+
+            // Import values that match fields selected from the preview page.
+            foreach ($fieldinfo as $key => $value) {
+                $field = $fields[$value];
+                $content = new stdClass();
+                $content->fieldid = $field->id;
+                $content->recordid = $recordid;
+                if ($field->type == 'textarea') {
+                    // the only field type where HTML is possible
+                    $value = clean_param($record[$key], PARAM_CLEANHTML);
+                } else {
+                    // remove potential HTML:
+                    $patterns[] = '/</';
+                    $replacements[] = '&lt;';
+                    $patterns[] = '/>/';
+                    $replacements[] = '&gt;';
+                    $value = preg_replace($patterns, $replacements, $record[$key]);
+                }
+
+                // for now, only for "latlong" and "url" fields, but that should better be looked up from
+                // $CFG->dirroot . '/mod/data/field/' . $field->type . '/field.class.php'
+                // once there is stored how many contents the field can have.
+                if (preg_match("/^(latlong|url)$/", $field->type)) {
+                    $values = explode(" ", $value, 2);
+                    $content->content  = $values[0];
+                    // The url field doesn't always have two values (unforced autolinking).
+                    if (count($values) > 1) {
+                        $content->content1 = $values[1];
+                    }
+                } else {
+                    $content->content = $value;
+                }
+
+                $oldcontent = $DB->get_record('data_content', array('fieldid'=>$field->id, 'recordid'=>$recordid));
+                $content->id = $oldcontent->id;
+                $DB->update_record('data_content', $content);
+            }
+            $recordsadded++;
+            print get_string('added', 'moodle', $recordsadded) . ". " . get_string('entry', 'data') . " (ID $recordid)<br />\n";
+        }
+    }
+    $cir->close();
+    $cir->cleanup(true);
+
+    if ($recordsadded > 0) {
+        echo $OUTPUT->notification($recordsadded. ' '. get_string('recordssaved', 'data'), '');
+    } else {
+        echo $OUTPUT->notification(get_string('recordsnotsaved', 'data'), 'notifysuccess');
+    }
+
+    echo $OUTPUT->continue_button('import.php?d='.$data->id);
+    // }
+} else {
+    $form2->display();
+}
+// Finish the page.
 echo $OUTPUT->footer();
--- mod/data/import_form.php
+++ mod/data/import_form.php
@@ -1,4 +1,26 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Data module import form.
+ *
+ * @copyright 2005 Martin Dougiamas  http://dougiamas.com
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @package mod-data
+ */
 if (!defined('MOODLE_INTERNAL')) {
     die('Direct access to this script is forbidden.');    ///  It must be included from a Moodle page
 }
@@ -25,11 +47,66 @@ class mod_data_import_form extends moodleform {
         $mform->addElement('select', 'encoding', get_string('fileencoding', 'mod_data'), $choices);
         $mform->setDefault('encoding', 'UTF-8');
 
+        $choices = array('10'=>10, '20'=>20, '100'=>100, '1000'=>1000, '100000'=>100000);
+        $mform->addElement('select', 'previewrows', get_string('rowpreviewnum', 'mod_data'), $choices);
+        $mform->setType('previewrows', PARAM_INT);
+
         $submit_string = get_string('submit');
         // data id
         $mform->addElement('hidden', 'd');
         $mform->setType('d', PARAM_INT);
 
         $this->add_action_buttons(false, $submit_string);
+    }
+}
+
+/**
+ * Database module field matching form.
+ */
+class mod_data_import_form2 extends moodleform {
+
+    function definition() {
+        global $CFG;
+
+        $mform =& $this->_form;
+        $columns = $this->_customdata['columns'];
+        $fields = $this->_customdata['fields'];
+        $data = $this->_customdata['formdata'];
+
+        $maptooptions = array('none' => 'none');
+        foreach ($columns as $key => $value) {
+            $value = trim($value);
+            $maptooptions[$key] = s($value);
+        }
+
+        $mform->addElement('header', 'fieldmappings', get_string('fieldmappings', 'mod_data'));
+
+        foreach ($fields as $fieldname => $field) {
+            // Picture and file fields can not be imported into the database.
+            if ($field->type != 'picture' && $field->type != 'file') {
+                $fieldname = trim($fieldname);
+                $mform->addElement('select', 'fieldmap_'. s($fieldname) . '_' . $field->id, $fieldname . ' (' . get_string($field->type, 'mod_data') . ')', $maptooptions);
+                $fieldmatch = array_search($fieldname, $columns);
+                if ($fieldmatch !== false) {
+                    $mform->setDefault('fieldmap_'. s($fieldname) . '_' . $field->id, $fieldmatch);
+                } else {
+                    $mform->setDefault('fieldmap_'. s($fieldname) . '_' . $field->id, 'none');
+                }
+            }
+        }
+
+        // Hidden fields.
+        // Import ID.
+        $mform->addElement('hidden', 'iid');
+        $mform->setType('iid', PARAM_INT);
+
+        // Data id.
+        $mform->addElement('hidden', 'd');
+        $mform->setType('d', PARAM_INT);
+
+        $this->set_data($data);
+
+        $submit_string = get_string('submit');
+        $this->add_action_buttons(true, $submit_string);
     }
 }
