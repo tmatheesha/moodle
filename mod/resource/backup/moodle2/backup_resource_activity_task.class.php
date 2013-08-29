5ff80612c4e92936e3f442ba877fda30f75892ef
Adrian Greeve <adrian@moodle.com>
2013-08-29 13:43:32 +0800

    MDL-32862 - Backup: Fix up of comments and doc block, small change to static variable.

--- mod/resource/backup/moodle2/backup_resource_activity_task.class.php
+++ mod/resource/backup/moodle2/backup_resource_activity_task.class.php
@@ -33,6 +33,11 @@ require_once($CFG->dirroot . '/mod/resource/backup/moodle2/backup_resource_steps
 class backup_resource_activity_task extends backup_activity_task {
 
     /**
+     * @param bool $resourceoldexists True if there are records in the resource_old table.
+     */
+    protected static $resourceoldexists = null;
+
+    /**
      * Define (add) particular settings this activity can have
      */
     protected function define_my_settings() {
@@ -49,53 +54,75 @@ class backup_resource_activity_task extends backup_activity_task {
 
     /**
      * Code the transformations to perform in the activity in
-     * order to get transportable (encoded) links
+     * order to get transportable (encoded) links.
+     *
+     * @param string $content some HTML text that eventually contains URLs to the activity instance scripts.
+     * @return string the content with the URLs encoded.
      */
     static public function encode_content_links($content) {
         global $CFG, $DB;
 
         $base = preg_quote($CFG->wwwroot,"/");
 
-        // Link to the list of resources
+        // Link to the list of resources.
         $search="/(".$base."\/mod\/resource\/index.php\?id\=)([0-9]+)/";
         $content= preg_replace($search, '$@RESOURCEINDEX*$2@$', $content);
 
-        // Link to resource view by moduleid
-        $search="/(".$base."\/mod\/resource\/view.php\?id\=)([0-9]+)/";
+        // Link to resource view by moduleid.
+        $search = "/(".$base."\/mod\/resource\/view.php\?id\=)([0-9]+)/";
+        // Link to resource view by recordid
+        $search2 = "/(".$base."\/mod\/resource\/view.php\?r\=)([0-9]+)/";
 
-        static $resource_old_exists = null;
-
-        // Check whether there are contents in the resource old table
-        if ($resource_old_exists === null) {
-            $resource_old_exists = $DB->record_exists('resource_old', array());
+        // Check whether there are contents in the resource old table.
+        if (static::$resourceoldexists === null) {
+            static::$resourceoldexists = $DB->record_exists('resource_old', array());
         }
 
         // If there are links to items in the resource_old table, rewrite them to be links to the correct URL
-        // for their new module
-        if ($resource_old_exists) {
+        // for their new module.
+        if (static::$resourceoldexists) {
+            // Match all of the resources.
             $result = preg_match_all($search, $content, $matches, PREG_PATTERN_ORDER);
 
-            // No resource links
+            // Course module ID resource links.
+            if ($result) {
+                list($insql, $params) = $DB->get_in_or_equal($matches[2]);
+                $oldrecs = $DB->get_records_select('resource_old', "cmid $insql", $params, '', 'cmid, newmodule');
+
+                for ($i = 0; $i < count($matches[0]); $i++) {
+                    $cmid = $matches[2][$i];
+                    if (isset($oldrecs[$cmid])) {
+                        // Resource_old item, rewrite it
+                        $replace = '$@' . strtoupper($oldrecs[$cmid]->newmodule) . 'VIEWBYID*' . $cmid . '@$';
+                    } else {
+                        // Not in the resource old table, don't rewrite
+                        $replace = '$@RESOURCEVIEWBYID*'.$cmid.'@$';
+                    }
+                    $content = str_replace($matches[0][$i], $replace, $content);
+                }
+            }
+
+            $matches = null;
+            $result = preg_match_all($search2, $content, $matches, PREG_PATTERN_ORDER);
+
+            // No resource links.
             if (!$result) {
                 return $content;
             }
-
+            // Resource ID links.
             list($insql, $params) = $DB->get_in_or_equal($matches[2]);
-            $oldrecs = $DB->get_records_select('resource_old', "cmid $insql", $params, '', 'cmid, newmodule');
+            $oldrecs = $DB->get_records_select('resource_old', "oldid $insql", $params, '', 'oldid, cmid, newmodule');
 
             for ($i = 0; $i < count($matches[0]); $i++) {
-                $cmid = $matches[2][$i];
-                if (isset($oldrecs[$cmid])) {
+                $recordid = $matches[2][$i];
+                if (isset($oldrecs[$recordid])) {
                     // Resource_old item, rewrite it
-                    $replace = '$@' . strtoupper($oldrecs[$cmid]->newmodule) . 'VIEWBYID*' . $cmid . '@$';
-                } else {
-                    // Not in the resource old table, don't rewrite
-                    $replace = '$@RESOURCEVIEWBYID*'.$cmid.'@$';
+                    $replace = '$@' . strtoupper($oldrecs[$recordid]->newmodule) . 'VIEWBYID*' . $oldrecs[$recordid]->cmid . '@$';
+                    $content = str_replace($matches[0][$i], $replace, $content);
                 }
-                $content = str_replace($matches[0][$i], $replace, $content);
             }
         } else {
-            $content= preg_replace($search, '$@RESOURCEVIEWBYID*$2@$', $content);
+            $content = preg_replace($search, '$@RESOURCEVIEWBYID*$2@$', $content);
         }
         return $content;
     }
