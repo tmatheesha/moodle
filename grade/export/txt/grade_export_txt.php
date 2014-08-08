1c8f3c0862ade9b71fd3e453657e73be3168b1ea
Adrian Greeve <adrian@moodle.com>
2014-08-08 10:42:29 +0800

    MDL-46599 gradebook: Added a time exported column to grade export via csv.
    This will be useful for checking if a csv import is trying to overwrite
    new grade updates.

--- grade/export/txt/grade_export_txt.php
+++ grade/export/txt/grade_export_txt.php
@@ -71,6 +71,9 @@ class grade_export_txt extends grade_export {
                 $exporttitle[] = $this->format_column_name($grade_item, true);
             }
         }
+
+        // Add the last modified column header.
+        $exporttitle[] = get_string('timeexported', 'gradeexport_txt');
         $csvexport->add_data($exporttitle);
 
         // Print all the lines of data.
@@ -103,6 +106,8 @@ class grade_export_txt extends grade_export {
                     $exportdata[] = $this->format_feedback($userdata->feedbacks[$itemid]);
                 }
             }
+            // Time exported.
+            $exportdata[] = time();
             $csvexport->add_data($exportdata);
         }
         $gui->close();
