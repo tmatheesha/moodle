dc09ebc7cbc3d46725c41367f46e9cff597ee6c8
Adrian Greeve <adrian@moodle.com>
2015-01-16 14:03:34 +0800

    ajax work

--- mod/lesson/yui/build/moodle-mod_lesson-pagemmove/moodle-mod_lesson-pagemmove.js
+++ mod/lesson/yui/build/moodle-mod_lesson-pagemmove/moodle-mod_lesson-pagemmove.js
@@ -24,12 +24,14 @@ var SELECTORS = {
 
     };
 
+var AJAXBASE = M.cfg.wwwroot + '/mod/lesson/ajax.php';
+
 Y.namespace('M.mod_lesson').PagemMove = Y.extend(PagemMove, Y.Base, {
 
     initializer: function() {
         var allLessonPages = Y.all(SELECTORS.PAGEMODULES);
         var lessonpagedata = Y.JSON.parse(this.get('lessondata'));
-        console.log(lessonpagedata);
+        // console.log(lessonpagedata);
 
         YUI().use('dd-delegate', 'dd-drop-plugin', function(Y) {
 
@@ -41,9 +43,57 @@ Y.namespace('M.mod_lesson').PagemMove = Y.extend(PagemMove, Y.Base, {
 
             del.on('drag:end', function(e) {
 
+                var currentnode = del.get('currentNode');
+                console.log(currentnode.getXY());
+
+                var ajaxurl = AJAXBASE,
+                    config;
+
+                config = {
+                    method: 'post',
+                    context: this,
+                    sync: false,
+                    data : {
+                        'action' : 'saveposition',
+                    },
+                    on: {
+                        success: function(tid, response) {
+                            var jsondata, quickcomment;
+                            try {
+                                jsondata = Y.JSON.parse(response.responseText);
+                                console.log(jsondata);
+                                // if (jsondata.error) {
+                                //     return new M.core.ajaxException(jsondata);
+                                // } else {
+                                //     console.log(jsondata);
+                                // }
+                            } catch (e) {
+                                //return new M.core.exception(e);
+                            }
+                        },
+                        failure: function(tid, response) {
+                            //return M.core.exception(response.responseText);
+                            alert('something went wrong');
+                        }
+                    }
+                };
+
+                YUI().use("io-base", function(Y) {
+                    Y.io(ajaxurl, config);
+                });
+
+
+                // console.log('got here');
+
+                // alert('got here');
+                // Save position.
+                // this._savedata();
+
+
                 // Keep it in the main container.
 
                 // Get x and y coordinates and update the database.
+
 
             });
 
@@ -62,6 +112,35 @@ Y.namespace('M.mod_lesson').PagemMove = Y.extend(PagemMove, Y.Base, {
             });
         });
 
+        this._redraw();
+
+    },
+
+    _savedata: function() {
+        // alert('saving data');
+        console.log('saving data');
+    },
+
+    _redraw: function() {
+
+        var lessonpagedata = Y.JSON.parse(this.get('lessondata'));
+        // console.log(lessonpagedata);
+        var lessonpagenodes = Y.all('.mod_lesson_page_element');
+        // console.log(lessonpagenodes);
+        // // Perhaps destroy the old one first.
+        // lessonpagenodes.each(function(node) {
+        //     console.log(node);
+        //     node.destroy();
+        // });
+
+        // lessonpagedata.each(function(lessonnode) {
+        //     console.log(lessonnode);
+        // });
+
+        // Draw a lesson page node.
+
+
+        // console.log('something');
     }
 
 
