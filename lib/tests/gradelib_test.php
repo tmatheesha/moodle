1a1007667e52f9868e44eaae9adbb55d3c2fcc90
Adrian Greeve <adrian@moodle.com>
2016-05-09 10:30:04 +0800

    MDL-21746 gradebook: Unit tests for grade rounding setting.

--- lib/tests/gradelib_test.php
+++ lib/tests/gradelib_test.php
@@ -30,6 +30,26 @@ require_once($CFG->libdir . '/gradelib.php');
 
 class core_gradelib_testcase extends advanced_testcase {
 
+    /**
+     * Define a local decimal separator.
+     *
+     * It is not possible to directly change the result of get_string in
+     * a unit test. Instead, we create a language pack for language 'xx' in
+     * dataroot and make langconfig.php with the string we need to change.
+     * The example separator used here is 'X'; on PHP 5.3 and before this
+     * must be a single byte character due to PHP bug/limitation in
+     * number_format, so you can't use UTF-8 characters.
+     */
+    protected function define_local_decimal_separator() {
+        global $SESSION, $CFG;
+
+        $SESSION->lang = 'xx';
+        $langconfig = "<?php\n\$string['decsep'] = 'X';";
+        $langfolder = $CFG->dataroot . '/lang/xx';
+        check_dir_exists($langfolder);
+        file_put_contents($langfolder . '/langconfig.php', $langconfig);
+    }
+
     public function test_grade_update_mod_grades() {
 
         $this->resetAfterTest(true);
@@ -55,5 +75,35 @@ class core_gradelib_testcase extends advanced_testcase {
         $modinstance->modname = 'assign';
 
         $this->assertTrue(grade_update_mod_grades($modinstance));
+    }
+
+    /**
+     * Test rounding grade values.
+     */
+    public function test_grade_round_value() {
+        // Special case for null.
+        $this->assertEquals('', grade_round_value(null));
+        // Default of 5 decimal places.
+        $this->assertEquals('5.43247', grade_round_value(5.43247));
+        $this->assertEquals('5.98374', grade_round_value(5.9837483));
+
+        // Custom number of decimal places.
+        $this->assertEquals('5.434', grade_round_value(5.4347, 3));
+        $this->assertEquals('64', grade_round_value(64.59999, 0));
+
+        // Tests Marina wanted put in.
+        $this->assertEquals('5.40', grade_round_value(5.4, 2));
+        $this->assertEquals('5.000', grade_round_value(5, 3));
+        $this->assertEquals('0.000', grade_round_value(0, 3));
+        $this->assertEquals('0.90', grade_round_value(0.9, 2));
+
+        // Tests with a localised decimal separator.
+        $this->define_local_decimal_separator();
+
+        $this->assertEquals('5X43247', grade_round_value(5.43247));
+        $this->assertEquals('24X87398', grade_round_value(24.87398473));
+        $this->assertEquals('33X3', grade_round_value(33.35, 1));
+        // No localisation.
+        $this->assertEquals('120.2345', grade_round_value(120.23455, 4, false));
     }
 }
